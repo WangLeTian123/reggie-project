6cb7cd93579ba2e075cf8559761aa9feb1302764
letian <letian@123.com>
2024-02-05 14:14:02 +0800

    开发了：登录过滤器、新增员工功能

--- src/main/java/com/itletian/controller/EmployeeController.java
+++ src/main/java/com/itletian/controller/EmployeeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @RestController
 @Slf4j
@@ -55,10 +56,28 @@ public class EmployeeController {
     /**
      * 员工退出功能
      */
-    @RequestMapping("/logout")
+    @PostMapping("/logout")
     public R<String> logout(HttpServletRequest request) {
         // 清理session中保存的当前登录员工的id
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
+    }
+
+    /**
+     * 新增员工功能
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Employee employee, HttpServletRequest request) {
+        // 默认初始密码是123456 需要md5加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        // 设置当前时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        // 将创建用户信息设置为该用户id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+        employeeService.save(employee);
+        return R.success("新增员工成功");
     }
 }
