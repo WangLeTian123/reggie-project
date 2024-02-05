cc6f3a4ac707baa9903b6eb39b513763efdf29de
letian <letian@123.com>
2024-02-05 20:49:52 +0800

    1、声明了一个全局异常处理类 2、使用mybatis-plus分页插件完成了分页查询功能

--- src/main/java/com/itletian/controller/EmployeeController.java
+++ src/main/java/com/itletian/controller/EmployeeController.java
@@ -1,16 +1,15 @@
 package com.itletian.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itletian.entity.Employee;
 import com.itletian.service.EmployeeService;
 import com.itletian.util.R;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
@@ -79,5 +78,26 @@ public class EmployeeController {
         employee.setUpdateUser(empId);
         employeeService.save(employee);
         return R.success("新增员工成功");
+    }
+
+    /**
+     * @param page: 当前页数
+     * @param pageSize: 每页最大记录数
+     * @param name: 根据名字查询信息的名字（模糊查询实现）
+     * @description 员工信息分页查询
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        // 创建分页构造器
+        Page<Employee> pageParams = new Page<>(page, pageSize);
+        // 创建条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        //添加过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);
+        //添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+        // 执行查询
+        employeeService.page(pageParams, queryWrapper);
+        return R.success(pageParams);
     }
 }
---
+++ src/main/java/com/itletian/util/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.itletian.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * 统一异常处理：全局异常处理类
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        log.error(ex.getMessage());
+
+        if (ex.getMessage().contains("Duplicate entry")) {
+            String[] split = ex.getMessage().split(" ");
+            String msg = split[2] + "已存在";
+            return R.error(msg);
+        }
+        return R.error("未知错误");
+    }
+}
